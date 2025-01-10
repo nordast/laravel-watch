e29b6a4ff4b61d4229e587f583ce1d8cc84b165b
nordast <nordast@gmail.com>
2025-01-10 13:04:01 +0200

    lesson

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -13,5 +14,19 @@ class Lesson extends Model
     public function course(): BelongsTo
     {
         return $this->belongsTo(Course::class);
+    }
+
+    protected function previous(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->course->lessons->firstWhere('number', $this->number - 1)
+        );
+    }
+
+    protected function next(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => $this->course->lessons->firstWhere('number', $this->number + 1)
+        );
     }
 }
--- resources/views/pages/lessons/show.blade.php
+++ resources/views/pages/lessons/show.blade.php
@@ -1,4 +1,3 @@
-
 <x-layout>
     <section class="pt-32 px-10">
         <div class="flex flex-col gap-8 container pb-24">
@@ -6,18 +5,26 @@
             <div class="flex flex-col gap-2 w-1/2 mx-auto">
                 <x-lesson.player :$lesson />
 
-
                 <div class="flex gap-2">
-                    <a href="#" class="rounded-md px-4 py-2 bg-gray-100 hover:bg-gray-200">
-                        <x-icon name="chevron-left" class="size-6"/>
-                    </a>
-                    <a href="#" class="rounded-md px-4 py-2 bg-gray-100 hover:bg-gray-200 mr-auto">
-                        <x-icon name="chevron-right" class="size-6"/>
-                    </a>
+                    <div class="flex gap-2 mr-auto">
+                        @if ($lesson->previous)
+                            <a href="{{ route('lessons.show', $lesson->previous) }}" class="rounded-md px-4 py-2 bg-gray-100 hover:bg-gray-200">
+                                <x-icon name="chevron-left" class="size-6"/>
+                            </a>
+                        @endif
+
+                        @if ($lesson->next)
+                            <a href="{{ route('lessons.show', $lesson->next) }}" class="rounded-md px-4 py-2 bg-gray-100 hover:bg-gray-200">
+                                <x-icon name="chevron-right" class="size-6"/>
+                            </a>
+                        @endif
+                    </div>
+
                     <a href="{{ route('courses.show', $lesson->course) }}"
                        class="font-semibold rounded-md px-4 py-2 bg-gray-100 hover:bg-gray-200">
                         All Lessons
                     </a>
+
                     <a href="#"
                        target="_blank"
                        class="font-semibold rounded-md px-4 py-2 bg-gray-100 hover:bg-gray-200">
